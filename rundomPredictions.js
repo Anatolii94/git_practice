923f7e8f9720975da2c90d5dbda50cb318ce38ac
Anatolii Korzh <korzhanatolii94@gmail.com>
2023-10-23 16:20:17 -0400

    First and last version code. Code get one of 5 rundom predictions

---
+++ rundomPredictions.js
@@ -0,0 +1,3 @@
+const messages = ['You will be lucky today!', 'You will meet your soulmate!', 'Do something for the benefit of society!', 'Be careful with new offers!', 'Give more time to your relatives!'];
+const rundomNum = Math.floor(Math.random() * 4);
+console.log(messages[rundomNum]);
